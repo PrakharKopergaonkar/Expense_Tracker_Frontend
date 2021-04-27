f4b5f970850638e08da04e0220b7c7adcc0103bd
rohanJa <rohan.jain150300@gmail.com>
2021-04-27 13:43:08 +0530

    added new expense and main router component

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Signup from './user/Signup'
 import Signin from './auth/Signin'
+import PrivateRoute from './auth/PrivateRoute'
 import Menu from './core/Menu'
 import Home from './core/Home'
-import Temp from './auth/temp'
+import ExpenseOverview from './expense/ExpenseOverview'
+import NewExpense from './expense/NewExpense'
 
 const MainRouter = () => {
     return (<div>
@@ -13,7 +15,9 @@ const MainRouter = () => {
         <Route exact path="/" component={Home}/>
         <Route path="/signup" component={Signup}/>
         <Route path="/signin" component={Signin}/>
-        <Route path="/temp" component={Temp}/>
+        <Route path="/expense" component={ExpenseOverview}/>
+        {/* <Route path="/temp" component={Temp}/> */}
+        <PrivateRoute path="/expenses/new" component={NewExpense}/>
       </Switch>
     </div>)
 }
