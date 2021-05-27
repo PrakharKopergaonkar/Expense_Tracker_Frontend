94f81177d11cbdcd37c6dda711225d47693c43de
rohanJa <rohan.jain150300@gmail.com>
2021-05-27 14:23:11 +0530

    report route added

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -7,6 +7,7 @@ import Menu from './core/Menu'
 import Home from './core/Home'
 import ExpenseOverview from './expense/ExpenseOverview'
 import NewExpense from './expense/NewExpense'
+import Reports from './report/Reports'
 
 const MainRouter = () => {
     return (<div>
@@ -17,10 +18,11 @@ const MainRouter = () => {
         <Route path="/signin" component={Signin}/>
         <Route path="/expense" component={ExpenseOverview}/>
         {/* <Route path="/temp" component={Temp}/> */}
+        <PrivateRoute path="/expenses/reports" component={Reports}/>
         <PrivateRoute path="/expenses/new" component={NewExpense}/>
         <PrivateRoute path="/expenses/all" component={Expenses}/>
       </Switch>
     </div>)
 }
 
-export default MainRouter
+export default MainRouter;
